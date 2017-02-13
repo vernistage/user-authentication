dc129d30328c6d1417a5aa62409256bf3e3655e2
vernistage <sdickers10@gmail.com>
2017-02-13 15:25:19 -0600

    correct tests

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -1,28 +1,43 @@
 require 'rails_helper'
 
 describe Job do
-  let(:job) { Job.create!({
+  let(:developer) { Job.create!({
     name: "developer",
     description: "writing code",
     status: 0,
     employment_type: 0
     }) }
 
+  let(:engineer) { Job.create!({
+    name: "engineer",
+    description: "shipping code",
+    status: 1,
+    employment_type: 1
+    }) }
+
   describe "job attributes" do
     it "job has a name" do
-      expect(job.name).to eq "developer"
+      expect(developer.name).to eq "developer"
     end
 
     it "job has a description" do
-      expect(job.description).to eq "writing code"
+      expect(developer.description).to eq "writing code"
     end
 
     it "job has a status" do
-      expect(job.status).to eq 0
+      expect(developer.status).to eq "active"
+    end
+
+    it "job has a status" do
+      expect(engineer.status).to eq "inactive"
     end
 
     it "job has an employement type" do
-      expect(job.employment_type).to eq 0
+      expect(developer.employment_type).to eq "full_time"
+    end
+
+    it "job has an employement type" do
+      expect(engineer.employment_type).to eq "part_time"
     end
   end
 

ecdf496230e468c56cd80f678d590e1a5466c749
vernistage <sdickers10@gmail.com>
2017-02-16 08:44:41 -0600

    finish basic controller specs for jobs

--- spec/controllers/job_spec.rb
+++ spec/controllers/job_spec.rb
@@ -12,12 +12,32 @@ describe JobsController, :type => :controller do
       get :index
       expect(response).to render_template("index")
     end
-
-    # it "loads all of the posts into @posts" do
-    #   post1, post2 = Post.create!, Post.create!
-    #   get :index
-    #
-    #   expect(assigns(:posts)).to match_array([post1, post2])
-    # end
   end
+
+  describe "GET #new" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :new
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the new job template" do
+      get :new
+      expect(response).to render_template("new")
+    end
+  end
+
+  describe "GET #show" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :show
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "shows a specific job" do
+      get :show
+      expect(response).to render_template("show")
+    end
+  end
+
 end
